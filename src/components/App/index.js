5708ca22cbb3b843563d3d38856b6233a0612e9a
Kenneth Tubman <tubofpower@gmail.com>
2018-09-12 20:41:03 -0400

    Added the equipment images and going to start linking to data soon

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -31,6 +31,9 @@ const Header = () => (
 			  <li className="nav-item">
 				  <NavLink exact to="/about" className="nav-link" activeClassName="nav-link active">About</NavLink>
 			  </li>
+			  <li className="nav-item">
+				  <NavLink exact to="/equipment" className="nav-link" activeClassName="nav-link active">Equipment</NavLink>
+			  </li>
 			</ul>
 		  </div>
 		</nav>
@@ -42,6 +45,7 @@ const Main = () => (
 		<Switch>
 			<Route exact path="/" component={Home} />
 			<Route path="/about" component={About} />
+			<Route path="/equipment" component={Equipment} />
 		</Switch>
 	</div>
 );
@@ -51,6 +55,26 @@ const Home = () => (
 		<h3>Home</h3>
 		<hr />
 		<p></p>
+	</div>
+);
+
+const Equipment = () => (
+	<div>
+		<img src="../../../static/img/Equipment_slots.png" usemap="#image-map"/>
+
+		<map name="image-map">
+			<area target="" alt="Cape" title="Cape" href="" coords="51,75,15,39" shape="rect"/>
+			<area target="" alt="Body" title="Body" href="" coords="56,78,92,114" shape="rect"/>
+			<area target="" alt="Legs" title="Legs" href="" coords="56,118,92,154" shape="rect"/>
+			<area target="" alt="Feet" title="Feet" href="" coords="56,158,92,194" shape="rect"/>
+			<area target="" alt="Head" title="Head" href="" coords="56,0,92,36" shape="rect"/>
+			<area target="" alt="Neck" title="Neck" href="" coords="56,39,92,75" shape="rect"/>
+			<area target="" alt="Shield" title="Shield" href="" coords="112,78,148,114" shape="rect"/>
+			<area target="" alt="Ammunition" title="Ammunition" href="" coords="97,39,133,75" shape="rect"/>
+			<area target="" alt="Weapon" title="Weapon" href="" coords="0,78,36,114" shape="rect"/>
+			<area target="" alt="Hands" title="Hands" href="" coords="0,158,36,194" shape="rect"/>
+			<area target="" alt="Ring" title="Ring" href="" coords="112,158,148,194" shape="rect"/>
+		</map>
 	</div>
 );
 
